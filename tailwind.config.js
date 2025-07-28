f4ab6561929b45cfb6a911f45960deed7aa732f0
Adam Carlile <hello@adamcarlile.com>
2025-07-28 23:53:44 +0100

    update layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,14 @@
-const colors = require("tailwindcss/colors");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.html'],
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.html",
+    "./*.md"
+  ],
   theme: {
-    fontFamily: {
-      trajan: ["trajan-pro", "sans-serif"],
-    },
-    extend: {
-      animation: {
-        "spin-slow": "spin 3s linear infinite",
-      },
-      backgroundImage: (theme) => ({
-        hero: "url('/imgs/tot21_bk_hero.jpg')",
-        swirl: "url('/imgs/swirl.png')",
-        halloweenbtn: "url('/imgs/halloween_btn_bg.png')",
-        index: "url('/imgs/tot21_index_hero.jpg')",
-      }),
-    },
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      gray: colors.neutral,
-      black: colors.black,
-      white: colors.white,
-      primary: colors.orange,
-      indigo: colors.indigo,
-      totblue: {
-        DEFAULT: "#50ced4",
-      },
-      totpink: {
-        DEFAULT: "#8d005d",
-      },
-    },
-  },
-  variants: {
     extend: {},
-    variants: {
-      animation: ['motion-safe']
-    }
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ]
+  plugins: [],
 }
